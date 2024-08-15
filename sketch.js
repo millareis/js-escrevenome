b4d51cca052bdaf53f9037153b073ade4806c8f3
millareis <birnfeld.camila@escola.pr.gov.br>
2024-08-14 23:14:04 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+    background ("black")
+  }
+  
+  function draw() {
+  
+    stroke ("Green");
+    fill ("purple");
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 25)}
+  
+  
+  }
